cb5ea2d1b41ee377edb3ee0662c65b5ec9d3908a
Arvind Vishwakarma <arvindvishwakrma37@gmail.com>
2026-05-06 02:20:41 +0530

    initializing devpulse_repo

---
+++ DevPulse_Client/src/app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const newVar = "arvind"
+
+const page = () => {
+  return (
+    <div className='w-full h-screen bg-zinc-700'>
+      My next app {newVar}
+    </div>
+  )
+}
+
+export default page
