fc5f8f5c8d01172af92c2c1ea220856bc0c178af
Arvind Vishwakarma <arvindvishwakrma37@gmail.com>
2026-05-08 01:37:37 +0530

    feat(auth): completed auth module

--- DevPulse_Server/src/app.ts
+++ DevPulse_Server/src/app.ts
@@ -1,5 +1,10 @@
 import express, { Request, Response } from 'express';
+import authRoutes from './modules/auth/auth.routes';
+
 const app = express();
+
+app.use(express.json());
+app.use('/v1/api/auth', authRoutes);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello from TypeScript Backend');
